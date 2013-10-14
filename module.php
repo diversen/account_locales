b445044e2adcd633b94f8fbdbd527c9fc0c4f3fd
dennis <dennis@dennis-laptop>
2013-10-14 15:36:37 +0200

    clean up.

--- module.php
+++ module.php
@@ -1,14 +1,30 @@
 <?php
 
+/**
+ * file contains account:locales
+ */
+
+/**
+ * class account_locales
+ */
 class account_locales {
     
+    /**
+     * events: attach the account_locales module to a parent module
+     * @param type $args
+     * @return type
+     */
     public static function events ($args) {
-        
         if ($args['action'] == 'attach_module_menu') {
             return self::getModuleMenuItem($args);
         }
     }
     
+    /**
+     * create a module menu item for setting locales per account
+     * @param type $args
+     * @return type
+     */
     public static function getModuleMenuItem ($args) {
         
         $ary = array(
@@ -18,11 +34,18 @@ class account_locales {
         return $ary;
     }
     
+    /**
+     * use locales language form and set 'account_locales_language', user_id 
+     * in system_cache
+     */
     public static function editAction () {
-        moduleloader::includeModule('locales');
-             
-        layout::setCurrentModuleMenu('account_locales', 'account_profile');
-        layout::setModuleMenu('account_profile');
+        
+        $parent = config::getModuleIni('account_locales_parent');
+        
+        moduleloader::includeModule('locales');         
+        layout::setCurrentModuleMenu('account_locales', $parent);
+        layout::setModuleMenu($parent);
+        
 
         if (isset($_POST['language'])) {
             locales::updateAccountLanguage('/account_locales/edit');
@@ -36,16 +59,5 @@ class account_locales {
         }
         locales::displaySetLanguage($default);
         
-    }
-
-    
-    public static function runLevel ($level) {
-        /*
-        if ($level == 4) {
-            $user_language = cache::get('account_locales_language', session::getUserId());
-            if ($user_language) {
-                config::setMainIni('language_account', $user_language);
-            }
-        }*/
     }
 }
